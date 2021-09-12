61921d59a079d2d1691a47e736d03f0c500199fb
YUHEI <76258455+yuehi@users.noreply.github.com>
2021-09-12 14:25:21 +0900

    変数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数の上書き";
+// console.log(val2);
+
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const変数を上書き"；
+
+// const val3 = "const変数を再宣言";
+
+// const val4 = {
+//   name: "ゆうへい",
+//   age: 28
+// };
+// val4.name = "yuhei";
+// val4.addres = "hahiouji";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monky");
+console.log(val5);
